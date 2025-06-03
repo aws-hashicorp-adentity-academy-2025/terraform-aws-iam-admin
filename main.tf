3ffef52e1fec5be086e1196a081f32ccbdc27050
Great-Stone <hahohh@gmail.com>
2025-06-03 16:15:10 +0900

    main.tf 파일에서 IAM 사용자 이름에 postfix 변수를 추가하고, IAM 정책의 Resource 필드를 명시적으로 설정하여 모든 리소스에 대한 접근을 허용하도록 수정했습니다.

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
 resource "aws_iam_policy" "admin" {
   name = "admin"
+  path        = "/"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action = "*"
         Effect = "Allow"
+        Resource = "*"
       }
     ]
-    Resource = "*"
   })
 }
 
 resource "aws_iam_user" "admin" {
-  name = "admin"
+  name = "admin-${var.postfix}"
 }
 
 resource "aws_iam_user_policy_attachment" "admin" {
